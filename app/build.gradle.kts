3073b3d9aec3c8325423729e7f02166bbca2b45b
Darshan Pania <darshan@clevertap.com>
2024-05-10 10:59:56 +0530

    Updates version code for Closed Testing Bundle and adds PRIVACY.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.darshan.notificity"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
