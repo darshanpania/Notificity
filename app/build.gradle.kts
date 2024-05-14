d17c458b7b1f78a7ecc01145be685df69956c7f2
Darshan Pania <darshan@clevertap.com>
2024-05-14 23:24:12 +0530

    Updates version & version code for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.darshan.notificity"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
