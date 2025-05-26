1119ce51e0e4e2cf6c6a23d153fe87ffee92f2de
shivamsharma <shivamsharma654.ss@gmail.com>
2025-05-26 21:23:22 +0530

    Add firebase-messaging dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-crashlytics")
-
+    implementation("com.google.firebase:firebase-messaging")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
