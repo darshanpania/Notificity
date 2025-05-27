c48672324bd4041f44c0d8d9169cbc7388c6bd46
shivamsharma <shivamsharma654.ss@gmail.com>
2025-05-27 16:11:29 +0530

    Rename Alert's dismiss button

--- app/src/main/java/com/darshan/notificity/MainActivity.kt
+++ app/src/main/java/com/darshan/notificity/MainActivity.kt
@@ -286,7 +286,7 @@ class MainActivity : ComponentActivity() {
             },
             dismissButton = {
                 TextButton(onClick = { onDismiss() }) {
-                    Text("Cancel")
+                    Text("Later")
                 }
             }
         )
