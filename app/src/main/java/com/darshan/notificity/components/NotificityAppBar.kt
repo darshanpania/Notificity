b09ac9a8f05e23d6254ce459a69e46fc65577c1a
shivamsharma <shivamsharma654.ss@gmail.com>
2025-05-29 19:15:27 +0530

    Create AppBar component

---
+++ app/src/main/java/com/darshan/notificity/components/NotificityAppBar.kt
@@ -0,0 +1,46 @@
+package com.darshan.notificity.components
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.darshan.notificity.DarkGrey
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NotificityAppBar(
+    title: String,
+    actions: @Composable (RowScope.() -> Unit)? = null
+) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+
+    val isDark = isSystemInDarkTheme()
+
+    val topBarColors = TopAppBarDefaults.topAppBarColors(
+        containerColor = if (isDark) DarkGrey else Color.White,
+        titleContentColor = if (isDark) Color.White else DarkGrey,
+    )
+
+    Surface(
+        tonalElevation = 4.dp,
+        shadowElevation = 4.dp,
+    ) {
+        TopAppBar(
+            title = { Text(text = title) },
+            actions = {
+                actions?.invoke(this)
+            },
+            scrollBehavior = scrollBehavior,
+            colors = topBarColors
+        )
+    }
+}
+
+
