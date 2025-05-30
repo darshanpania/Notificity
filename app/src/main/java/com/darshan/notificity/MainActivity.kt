c4e22360350a2569fc9834cb4293330f6f096dc5
shivamsharma <shivamsharma654.ss@gmail.com>
2025-05-30 17:14:12 +0530

    remove unused code

--- app/src/main/java/com/darshan/notificity/MainActivity.kt
+++ app/src/main/java/com/darshan/notificity/MainActivity.kt
@@ -45,7 +45,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -190,7 +189,6 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun NotificityApp(viewModel: MainViewModel) {
         val isPermissionGranted by viewModel.isNotificationPermissionGranted.collectAsState()
-        val context = LocalContext.current
 
         Scaffold(
             topBar = {
