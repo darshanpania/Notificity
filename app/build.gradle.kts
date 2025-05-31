e56845dcb3b2d1a1b25c0317c92b1071d4221a6d
shivamsharma <shivamsharma654.ss@gmail.com>
2025-05-31 19:31:30 +0530

    Increment versionName and versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.darshan.notificity"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
