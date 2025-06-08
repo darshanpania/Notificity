f1fbc8484bbb72fb7a3a4138c371e6603fe8a543
shivamsharma <shivamsharma654.ss@gmail.com>
2025-06-08 21:51:47 +0530

    Update versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 6
-        versionName = "1.1.1"
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
