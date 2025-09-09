dbdbf96d837366d31310fc825c6b36d296866c0d
anshsachdeva <anshsachdeva.work@gmail.com>
2025-09-10 00:42:18 +0530

    [Bugfix] add easy to scale sorting logic

--- app/src/main/java/com/darshan/notificity/NotificationDao.kt
+++ app/src/main/java/com/darshan/notificity/NotificationDao.kt
@@ -12,7 +12,7 @@ interface NotificationDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNotification(notification: NotificationEntity)
 
-    @Query("SELECT * FROM notification ORDER BY timestamp DESC")
+    @Query("SELECT * FROM notification")
     fun getAllNotificationsFlow(): Flow<List<NotificationEntity>>
 
     @Delete
--- app/src/main/java/com/darshan/notificity/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/darshan/notificity/main/viewmodel/MainViewModel.kt
@@ -49,6 +49,12 @@ constructor(
     val appInfoFromFlow: Flow<List<AppInfo>> =
         notificationsFlow.map { notifications ->
             notifications
+                .sortedByDescending { it ->
+                    it.timestamp
+                    // Normalize timestamp to the start of its hour (ignoring minutes/seconds)
+                    val millisPerHour = 1000 * 60 * 60
+                    it.timestamp / millisPerHour
+                }
                 .groupBy { it.packageName }
                 .map { entry ->
                     AppInfo(
